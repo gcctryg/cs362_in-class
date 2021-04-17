6c08313be4e18a8dcfb5bbc6e3f3f9dc0a0a7aff
Xuelu Feng <gcctryg@gmail.com>
2021-04-16 23:30:22 -0700

    password generator complete

--- pwd_generator.py
+++ pwd_generator.py
@@ -1,6 +1,11 @@
-print('Enter a number to generate a password: ')
-x = input()
-
 import string
 import random
+
+print('Enter a number to generate a password: ')
+x = input()
+x = int(x)
+
+res = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = x))
   
+print("The password is: " + str(res))
