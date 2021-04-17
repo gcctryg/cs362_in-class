972abc11c0057da72390164fd53f0a81d724b49a
Xuelu Feng <gcctryg@gmail.com>
2021-04-16 23:05:13 -0700

    complete cal

--- calc.py
+++ calc.py
@@ -1,10 +1,15 @@
-#summation
-def cal(a, b)
-    summation = a + b 
+def cal(a, b):
+    summation = a + b
+    print(summation)
     subtraction = a - b
+    print(subtraction)
     multiplication = a * b
+    print(multiplication)
     division = a / b
+    print(division)
 
-myList = [summation, subtraction, multiplication, division]
+    myList = [summation, subtraction, multiplication, division]
+    Sum = sum(myList)
+    return Sum
 
-Sum = sum(myList)
+print(cal(1,2))
